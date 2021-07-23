620657cc1f9a8fca6e92e96596c730fae32de258
shubham <shubhamna27@gmail.com>
2021-07-23 14:50:39 +0530

    sample commit two

---
+++ Subract.java
@@ -0,0 +1,14 @@
+public class Subract {
+
+
+        public static void main(String [] args)
+        {
+            int diff =0;
+            int a =40,b=30;
+            diff = a-b;
+            System.out.println("The Total difference is : "+diff);
+        }
+        
+        
+    
+}
